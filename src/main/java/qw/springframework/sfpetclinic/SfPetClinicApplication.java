87551dee8f2abd96359cbbf384560eca0d793f24
Qi Wu <qi.wu@elliemae.com>
2018-10-03 10:10:05 -0700

    Spring Framework Context

--- src/main/java/qw/springframework/sfpetclinic/SfPetClinicApplication.java
+++ src/main/java/qw/springframework/sfpetclinic/SfPetClinicApplication.java
@@ -2,11 +2,15 @@ package qw.springframework.sfpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import qw.springframework.sfpetclinic.controllers.MyController;
 
 @SpringBootApplication
 public class SfPetClinicApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SfPetClinicApplication.class, args);
+    ApplicationContext ctx = SpringApplication.run(SfPetClinicApplication.class, args);
+    MyController controller = (MyController) ctx.getBean("myController");
+    controller.hello();
   }
 }
