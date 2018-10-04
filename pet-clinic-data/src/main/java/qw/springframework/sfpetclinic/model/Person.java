28f252b904688a8970840630c3738987e1fc4888
Qi Wu <qi.wu@elliemae.com>
2018-10-04 13:25:46 -0700

    Implement Base Entity. Closes #11

--- pet-clinic-data/src/main/java/qw/springframework/sfpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/qw/springframework/sfpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package qw.springframework.sfpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/qw/springframework/sfpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/qw/springframework/sfpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package qw.springframework.sfpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
