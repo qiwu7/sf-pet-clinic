0422b4e653d018c95fb1021057dc5ac2bb38b275
Qi Wu <qi.wu@elliemae.com>
2018-10-10 11:02:02 -0700

    Refactor to use application properties file

--- src/main/java/qw/springframework/sfpetclinic/config/PropertyConfig.java
+++ src/main/java/qw/springframework/sfpetclinic/config/PropertyConfig.java
@@ -3,18 +3,10 @@ package qw.springframework.sfpetclinic.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import qw.springframework.sfpetclinic.examplebeans.FakeDataSource;
 import qw.springframework.sfpetclinic.examplebeans.FakeJmsBroker;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("${classpath:datasource.properties}"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
 
     @Value("${qw.username}")
@@ -43,10 +35,5 @@ public class PropertyConfig {
     @Bean
     public FakeJmsBroker fakeJmsBroker() {
         return new FakeJmsBroker(jmsUser, jmsPassword, jmsDbUrl);
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        return new PropertySourcesPlaceholderConfigurer();
     }
 }
